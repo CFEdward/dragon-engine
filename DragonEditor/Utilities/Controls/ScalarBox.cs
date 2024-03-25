977115d169da194d7da2fda05b124f442b2ee3fd
Eduard Ciofu <cfedward7@gmail.com>
2024-03-25 06:42:55 +0100

    NumberBox, ScalarBox, VectorBox

---
+++ DragonEditor/Utilities/Controls/ScalarBox.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace DragonEditor.Utilities.Controls
+{
+    class ScalarBox : NumberBox
+    {
+        static ScalarBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ScalarBox), new FrameworkPropertyMetadata(typeof(ScalarBox)));
+        }
+    }
+}
