d0f2681c7297841ba9cf4b18bb684c5032707401
Eduard Ciofu <cfedward7@gmail.com>
2024-03-30 00:58:04 +0100

    C++ scripting and script component

--- Engine/Utilities/Utilities.h
+++ Engine/Utilities/Utilities.h
@@ -5,17 +5,36 @@
 
 #if USE_STL_VECTOR
 #include <vector>
+#include <algorithm>
 namespace dragon::utl {
+
 template<typename T>
 using vector = std::vector<T>;
+
+template<typename T>
+void erase_unordered(std::vector<T>& v, size_t index)
+{
+	if (v.size() > 1)
+	{
+		std::iter_swap(v.begin() + index, v.end() - 1);
+		v.pop_back();
+	}
+	else
+	{
+		v.clear();
+	}
+}
+
 }
 #endif
 
 #if USE_STL_DEQUE
 #include <deque>
 namespace dragon::utl {
+
 template<typename T>
 using deque = std::deque<T>;
+
 }
 #endif
 
